be4adc1913f417ea0c4bcdeeb0c13ed6ba9421b4
Vivek-bhatt05 <vivekbhatt529@gmail.com>
2023-07-10 15:26:58 +0530

    done

--- my-app/helpers/apicalling.js
+++ my-app/helpers/apicalling.js
@@ -1,6 +1,6 @@
 export async function getAllEventsData(){
     try{
-        const res = await fetch('http://localhost:4400/graphql?query=books')
+        const res = await fetch('http://localhost:4400/graphql?query=%7B%0A%20%20books%20%7B%0A%20%20%20%20id%0A%20%20%20%20name%0A%20%20%7D%0A%7D')
         console.log(res);
         const data = await res.json()
 
